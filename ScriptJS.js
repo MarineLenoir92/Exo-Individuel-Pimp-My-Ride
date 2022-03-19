b2fcfd6aacc2b19cc24758e25137b6012f342b28
Marine <marine.lenoir@hotmail.fr>
2022-03-19 12:51:25 +0100

    Version Etape 2

--- ScriptJS.js
+++ ScriptJS.js
@@ -1,24 +1,42 @@
-let tripToParse = "Perdita 8 10 8";
+let trips =  [
+    "Roger 0 5 10",
+    "Pongo 3 7 14",
+    "Perdita 8 10 8",
+    "Anita 16 3 7",
+  ]
 
-function parseTrip(trip){
-//convert string to array//
-let array = trip.split(" ");
+//split an array in multiple array//
+function parseTrip(trips){
 
-console.log(array);
+    let clientUn = trips.slice(0,1).toString().split(" ");
+    console.log(clientUn);
+    
+    let clientTwo = trips.slice(1,2).toString().split(" ");
+    console.log(clientTwo);
+    
+    let clientThree = trips.slice(2,3).toString().split(" ");
+    console.log(clientThree);
+    
+    let clientFour = trips.slice(3,4).toString().split(" ");
+    console.log(clientFour);
 
-//append values to object//
-//key : value => array[i]//
-const client = {client: array[0]};
-console.log(client);
-const start = {start: array[1]};
-console.log(start);
-const duration = {duration: array[2]};
-console.log(duration);
-const price = {price: array[3]};
-console.log(price);
-
-const infoTrip = Object.assign(client, start, duration, price);
-console.log(infoTrip);
+//return multiple values//
+return[clientUn, clientTwo, clientThree, clientFour]
 }
 
-parseTrip(tripToParse);
+
+function parseTrips(clientUn, clientTwo, clientThree, clientFour){
+
+let clients = [clientUn, clientTwo, clientThree, clientFour]
+let keys = ["client", "start", "duration", "price"];
+let object = {};
+
+for (i=0; i<clients.length; i++){
+keys.forEach((element, index) => (object[element] = clients[i][index]));
+console.log(object);
+}
+}
+
+
+let [clientUn, clientTwo, clientThree, clientFour] = parseTrip(trips);
+parseTrips(clientUn, clientTwo, clientThree, clientFour);
